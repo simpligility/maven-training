0a18205a2280d70199acb87be71ce12d44b7d4dd
Manfred Moser <manfred@simpligility.com>
2014-12-05 23:00:38 -0800

    added print-pdf url parameter

--- makepdfs.sh
+++ makepdfs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./node_modules/phantomjs/lib/phantom/bin/phantomjs plugin/print-pdf/print-pdf.js "http://localhost:8000/installing-running-maven.html" takari-training-slides/TakariTraining-InstallingAndRunningMaven.pdf
+./node_modules/phantomjs/lib/phantom/bin/phantomjs plugin/print-pdf/print-pdf.js "http://localhost:8000/installing-running-maven.html?print-pdf" takari-training-slides/TakariTraining-InstallingAndRunningMaven.pdf
 ./node_modules/phantomjs/lib/phantom/bin/phantomjs plugin/print-pdf/print-pdf.js "http://localhost:8000/intro-to-maven.html?print-pdf" takari-training-slides/TakariTraining-IntroductionToMaven.pdf
